ab44fa8b4f8b5e1b75f44b7a97ab35c968cef678
FALCHETTO Marc <Revton@hotmail.fr>
2020-10-30 11:21:40 -0930

    Ajout windowsAdd + cs

--- App Recette Cuisine/WindowAdd.xaml.cs
+++ App Recette Cuisine/WindowAdd.xaml.cs
@@ -23,5 +23,53 @@ namespace App_Recette_Cuisine
         {
             InitializeComponent();
         }
+
+        public void Button_Click_AddClose(object sender, RoutedEventArgs e)
+        {
+            RecetteBuisness.Recette plat = new RecetteBuisness.Recette(0, titre.Text, categorie.Text, tempsprepa.Text, modeprepa.Text, nbrperson.Text,
+                                                        ingrédients.Text, url.Text, origine.Text);
+
+            if (RecetteBuisness.RecetteAcess.InsertRecette(plat))
+            {
+                MessageBox.Show("Les infos client ont bien été envoyés");
+            }
+            else
+            {
+                MessageBox.Show("Erreur, les infos client n'ont pas pu été envoyés");
+            }
+            this.Close();
+        }
+
+        public void Button_Click_Add(object sender, RoutedEventArgs e)
+        {
+            RecetteBuisness.Recette plat = new RecetteBuisness.Recette(0, titre.Text, categorie.Text, tempsprepa.Text, modeprepa.Text, nbrperson.Text,
+                                                        ingrédients.Text, url.Text, origine.Text);
+            //Buisness.clients.Add(phone);
+            if (RecetteBuisness.RecetteAcess.InsertRecette(plat))
+            {
+                MessageBox.Show("Les infos client ont bien été envoyés");
+            }
+            else
+            {
+                MessageBox.Show("Erreur, les infos client n'ont pas pu été envoyés");
+            }
+        }
+
+        private void MenuItem_Click_Clear(object sender, RoutedEventArgs e)
+        {
+            titre.Clear();
+            categorie.Clear();
+            tempsprepa.Clear();
+            modeprepa.Clear();
+            nbrperson.Clear();
+            ingrédients.Clear();
+            //url ou bouton add images?
+            origine.Clear();
+        }
+
+        private void MenuItem_Click_Close(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
